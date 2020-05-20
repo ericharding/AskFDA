75279548ab4b226aa1770e603ca58b642d6ece44
Eric Harding <eric.harding@bluetradesys.com>
2020-05-20 12:09:18 -0400

    runcommand

--- import/Program.fs
+++ import/Program.fs
@@ -1,8 +1,31 @@
-﻿// Learn more about F# at http://fsharp.org
-
+﻿open System.Diagnostics
 open System
 
-[<EntryPoint>]
-let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+let runCommand exe args =
+  let info = ProcessStartInfo()
+  info.FileName <- exe
+  info.Arguments <- args
+  info.UseShellExecute <- false
+  info.RedirectStandardOutput <- true
+  let proc = Process.Start(info)
+  let output = proc.StandardOutput.ReadToEnd()
+  proc.WaitForExit()
+  output
+
+let pdf2text path =
+  runCommand "pdf2txt" path
+
+// start in a folder full of zip files
+// run '7z x' on all files
+// recurse into all subdirectories
+// run pdf2txt on all files
+// slurp in text
+
+let ls = runCommand "ls" ""
+
+
+let args = 
+  Environment.GetCommandLineArgs()
+  |> Array.head
+
+printfn "%s" args
